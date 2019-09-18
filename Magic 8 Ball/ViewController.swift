d4c8a275e0cbcebb78560b2b3a4392ff2a650ba6
Bryan Klingler <bklingler89@gmail.com>
2019-09-18 18:20:30 -0400

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Bryan Klingler on 9/18/19.
+//  Copyright © 2019 Bryan Klingler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
